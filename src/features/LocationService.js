da3e66e290a30515483395ee64417e634d6c6e87
danniolsen <danni.r.olsen@gmail.com>
2020-03-20 14:42:18 +0100

    chenge 30 meters to 10 meters

--- src/features/LocationService.js
+++ src/features/LocationService.js
@@ -49,7 +49,7 @@ export const CheckImagesLocation = (prevLocation, imgLocation) => {
     { latitude: imgLocation.latitude, longitude: imgLocation.longitude }
   );
   console.log(result);
-  return result > 30 ? false : true;
+  return result > 10 ? false : true;
 };
 
 export default LocationService;
